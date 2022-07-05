66a94bdf94c74e5e11707961a6ac91f64b7e9133
mahamakhan <mahamimranamjad@gmail.com>
2022-07-05 08:26:21 -0700

    completed css

---
+++ script.js
@@ -0,0 +1,37 @@
+const characterList=[
+
+  {
+    name:'bpanther'
+    image:'blackpanther.jpg'
+  }
+  {
+    name:'bwidow'
+    image:'bwidow.jpg'
+  }
+  {
+    name:'captain'
+    image:'captain.jpg'
+  }
+  {
+    name:'drstrange'
+    image:'drstrange.jpg'
+  
+  }
+  {
+    name:'hulk'
+    image:'hulk.jpg'
+  }
+  {
+    name:'ironman'
+    image:'ironman.jpg'
+  }
+  {
+    name:'spiderman'
+    image:'spiderman.jpg'
+  
+  }
+  {
+    name:'thor'
+  image:'thor.jpg'
+}
+]
