a183dec57be0cc698589a646d6acbb1561e34524
mahamakhan <mahamimranamjad@gmail.com>
2022-07-08 09:23:19 -0700

    added timer but still have to make changes to it

--- script.js
+++ script.js
@@ -48,3 +48,43 @@ function darkMode() {
 const button = document.querySelector('.mode')
 
 button.addEventListener('click', darkMode)
+
+var countDownDate = new Date('Jan 5, 2024 15:37:25').getTime()
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime()
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+  // Display the result in the element with id="demo"
+  document.getElementById('demo').innerHTML = minutes + 'm ' + seconds + 's '
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x)
+    document.getElementById('demo').innerHTML = 'Time Up'
+  }
+}, 1000)
+
+// function hourglass() {
+//   var a
+//   a = document.getElementById('div1')
+//   a.innerHTML = '&#xf251;'
+//   setTimeout(function () {
+//     a.innerHTML = '&#xf252;'
+//   }, 1000)
+//   setTimeout(function () {
+//     a.innerHTML = '&#xf253;'
+//   }, 2000)
+// }
+// hourglass()
+// setInterval(hourglass, 3000)
